c5452bdf4682fc147bef1d90925a6f4a6d1f27c3
Md Mushfiqur Rahman <mushfiqur11@iut-dhaka.edu>
2024-03-13 07:07:29 -0400

    Added cost tracking

--- main.py
+++ main.py
@@ -6,9 +6,20 @@ from model import *
 if __name__ == "__main__":
     gpt_config = retrieve_yaml('gpt_config.yaml')
     prompt = retrieve_prompt('gpt_prompt.yaml')['simpleprob']
+    cost_limit = 0.5
 
-    task = 'Be the person you needed when you were younger.'
+    # task = 'Be the person you needed when you were younger.'
 
-    gpt_response = get_gpt_response(gpt_config, prompt, task, verbose=True, json_response=True)
+    # gpt_response = get_gpt_response(gpt_config, prompt, task, verbose=True, json_response=True)
 
-    print(gpt_response)
+    task_list = ['Be the person you needed when you were younger.', 'Be the person you needed when you were younger.', 'Be the person you needed when you were younger.']
+    result_list = []
+    total_cost = 0
+    for task in task_list:
+        gpt_response = get_gpt_response(gpt_config, prompt, task, verbose=True, json_response=True, get_cost=True)
+        response, cost = gpt_response
+        result_list.append(response)
+        total_cost += cost
+        if total_cost > cost_limit:
+            break
+    print(result_list)
